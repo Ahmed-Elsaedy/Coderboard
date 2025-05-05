a2ce179f80aff7730cb4ff0e9386aafd01c35e68
Ahmed Elsaedy <ahmedelsaedy825@hotmail.com>
2025-05-05 08:32:21 +0300

    Add and configure ef with sql server

---
+++ Codeboard.Api/Infrastructure/Database/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Codeboard.Api.Domain;
+using Microsoft.EntityFrameworkCore;
+
+namespace Codeboard.Api.Infrastructure.Database
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options) : base(options) { }
+
+        public DbSet<Student> Students => Set<Student>();
+    }
+}
--- Codeboard.Api/Program.cs
+++ Codeboard.Api/Program.cs
@@ -1,6 +1,11 @@
 using Codeboard.Api.Framework.Extenstions;
+using Codeboard.Api.Infrastructure.Database;
+using Microsoft.EntityFrameworkCore;
 
 var builder = WebApplication.CreateBuilder(args);
+
+builder.Services.AddDbContext<AppDbContext>(options =>
+    options.UseSqlServer(builder.Configuration.GetConnectionString("DefaultConnection")));
 
 builder.Services.AddControllers();
 builder.Services.AddOpenApi();
