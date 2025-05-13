cb096c8411e19f7284a3900f4f56eb5db33ad189
Ahmed Elsaeidy <asaeidy.c@tasheer.com>
2025-05-13 13:22:32 +0300

    Add HandlebarJs

---
+++ Coderboard.Web/TagHelpers/ButtonTagHelper.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Razor.TagHelpers;
+namespace Coderboard.Web.TagHelpers
+{
+    [HtmlTargetElement("custom-button")]
+    public class ButtonTagHelper : TagHelper
+    {
+        public string Text { get; set; } = "Click Me";
+        public string Color { get; set; } = "primary"; // bootstrap default
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            output.TagName = "button"; // Replace <custom-button> with <button>
+            output.Attributes.SetAttribute("class", $"btn btn-{Color}");
+            output.Content.SetContent(Text);
+        }
+    }
+}
---
+++ Coderboard.Web/TagHelpers/HandlebarsTemplateTagHelper.cs
@@ -0,0 +1,43 @@
+﻿using HandlebarsDotNet;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
+namespace Coderboard.Web.TagHelpers
+{
+    [HtmlTargetElement("handlebars-template")]
+    public class HandlebarsTemplateTagHelper : TagHelper
+    {
+        [HtmlAttributeName("id")]
+        public string Id { get; set; } = Guid.NewGuid().ToString("N");
+
+        [HtmlAttributeName("model")]
+        public object Model { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            var templateContent = (await output.GetChildContentAsync()).GetContent();
+
+            var compiled = Handlebars.Compile(templateContent);
+            var rendered = compiled(Model ?? new { });
+
+            string templateId = $"{Id}-template";
+            string containerId = $"{Id}-container";
+
+            output.TagName = null;
+            output.Content.Clear();
+
+            output.Content.AppendHtml($"""
+                <script id="{templateId}" type="text/x-handlebars-template">
+{templateContent}
+</script>
+""");
+
+            output.Content.AppendHtml(Environment.NewLine);
+
+            output.Content.AppendHtml($"""
+                <div id="{containerId}">
+{rendered}
+</div>
+""");
+        }
+    }
+}
