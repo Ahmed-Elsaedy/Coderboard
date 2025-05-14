8b63be4e0f324958eed6cad53d92a909e6fbbd1e
Ahmed Elsaeidy <asaeidy.c@tasheer.com>
2025-05-14 10:36:54 +0300

    Add draw page excalidraw component

---
+++ Coderboard.Web/Pages/Draw.cshtml.cs
@@ -0,0 +1,11 @@
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Coderboard.Web.Pages
+{
+    public class DrawModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
