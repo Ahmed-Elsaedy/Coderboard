e976f7a82772ec1277824b4b6ccc7c58546fb605
Ahmed Elsaedy <ahmedelsaedy825@hotmail.com>
2025-08-16 13:39:20 +0300

    Add ts support

---
+++ Coderboard.Modules.Identity.Web/Areas/Identity/Pages/Account/Login.cshtml.ts
@@ -0,0 +1,13 @@
+﻿class LoginPage {
+    constructor() {
+        console.log("Hello World");
+    }
+
+    public onLogin() {
+        // Logic for handling login
+        console.log("Login button clicked");
+    }
+}
+
+const loginPage = new LoginPage();
+loginPage.onLogin();
---
+++ Coderboard.Modules.Identity.Web/wwwroot/js/Identity/Pages/Account/Login.cshtml.js
@@ -0,0 +1,13 @@
+"use strict";
+class LoginPage {
+    constructor() {
+        console.log("Hello World");
+    }
+    onLogin() {
+        // Logic for handling login
+        console.log("Login button clicked");
+    }
+}
+const loginPage = new LoginPage();
+loginPage.onLogin();
+//# sourceMappingURL=Login.cshtml.js.map
